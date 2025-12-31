b2923748ffa972b0fd2039cb1be5c665cfcbcbb8
ChickenSir <shyguyman11@gmail.com>
2025-12-31 14:02:13 +1100

    Initial respository commit

---
+++ src/client/java/com/example/peculiarbiomes/PeculiarBiomesClient.java
@@ -0,0 +1,10 @@
+package com.example.peculiarbiomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PeculiarBiomesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/example/peculiarbiomes/PeculiarBiomesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.example.peculiarbiomes;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PeculiarBiomesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
